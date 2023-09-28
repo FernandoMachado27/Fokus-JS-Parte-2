52b49da4d8d15c622cf6d9ddce1b3f37fa377c18
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-09-28 12:59:48 -0300

    Implementando uma função

--- script-crud.js
+++ script-crud.js
@@ -9,7 +9,7 @@ let tarefas = [
         descricao: 'Tarefa Pendente',
         concluida:  false
     }
-]
+];
 
 const taskIconSvg = `<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
 fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -18,4 +18,22 @@ fill="none" xmlns="http://www.w3.org/2000/svg">
 <path
     d "M9 16.1719119.5938 5.57812121 6.9843819 18.9844L3.42188 13.4062L4.82812 12L19 16.17192"
     fill="#01080E" 
-/>`
+/>`;
+
+function createTask(tarefa) {
+    const li = document.createElement(); // criando item de lista no js para jogar no html
+    li.classList.add('app__section-task-list-item'); // adicionando uma claase para esta lista
+
+    const svgIcon = document.createElement('svg'); // criamos o icone que vai dentro da lista
+    svgIcon.innerHTML = taskIconSvg;
+
+    const paragraph = document.createElement('p'); // cria a partir da tag paragrafo
+    paragraph.classList.add('app__section-task-list-item-description');
+
+    paragraph.textContent = tarefa.descricao;
+
+    li.appendChild(svgIcon);
+    li.appendChild(paragraph);
+
+    return li;
+}
